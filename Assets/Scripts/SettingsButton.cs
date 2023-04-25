cc43c693352b5a2aa6ca86ccf930120d0daf29e4
Berke Konargöçer <berke.konargocer@gmail.com>
2023-04-25 23:22:23 +0300

    SettingsButton.cs

---
+++ Assets/Scripts/SettingsButton.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class SettingsButton : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        private void OnEnable()
+        {
+        }
+
+        private void OnDisable()
+        {
+        }
+
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+        public void SettingsButton()
+        {
+        }
+    }
+}
